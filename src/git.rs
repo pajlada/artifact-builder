b83d55f5bcf3d2ac4a5dd5d520db9437cb0e6c1e
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2023-04-23 13:40:03 +0200

    progress

---
+++ src/git.rs
@@ -0,0 +1,137 @@
+use anyhow::anyhow;
+use git2::Repository;
+
+use std::io::Write;
+
+#[allow(unused)]
+use tracing::log::*;
+
+pub fn fetch<'a>(
+    repo: &'a git2::Repository,
+    refs: &[&str],
+    remote: &'a mut git2::Remote,
+) -> Result<git2::AnnotatedCommit<'a>, git2::Error> {
+    let mut cb = git2::RemoteCallbacks::new();
+
+    // Print out our transfer progress.
+    cb.transfer_progress(|stats| {
+        if stats.received_objects() == stats.total_objects() {
+            print!(
+                "Resolving deltas {}/{}\r",
+                stats.indexed_deltas(),
+                stats.total_deltas()
+            );
+        } else if stats.total_objects() > 0 {
+            print!(
+                "Received {}/{} objects ({}) in {} bytes\r",
+                stats.received_objects(),
+                stats.total_objects(),
+                stats.indexed_objects(),
+                stats.received_bytes()
+            );
+        }
+        std::io::stdout().flush().unwrap();
+        true
+    });
+
+    let mut fo = git2::FetchOptions::new();
+    fo.remote_callbacks(cb);
+    // Always fetch all tags.
+    // Perform a download and also update tips
+    fo.download_tags(git2::AutotagOption::All);
+    println!("Fetching {} for repo", remote.name().unwrap());
+    remote.fetch(refs, Some(&mut fo), None)?;
+
+    // If there are local objects (we got a thin pack), then tell the user
+    // how many objects we saved from having to cross the network.
+    let stats = remote.stats();
+    if stats.local_objects() > 0 {
+        println!(
+            "\rReceived {}/{} objects in {} bytes (used {} local \
+             objects)",
+            stats.indexed_objects(),
+            stats.total_objects(),
+            stats.received_bytes(),
+            stats.local_objects()
+        );
+    } else {
+        println!(
+            "\rReceived {}/{} objects in {} bytes",
+            stats.indexed_objects(),
+            stats.total_objects(),
+            stats.received_bytes()
+        );
+    }
+
+    let fetch_head = repo.find_reference("FETCH_HEAD")?;
+    repo.reference_to_annotated_commit(&fetch_head)
+}
+
+fn fast_forward(
+    repo: &Repository,
+    lb: &mut git2::Reference,
+    rc: &git2::AnnotatedCommit,
+) -> Result<(), git2::Error> {
+    let name = match lb.name() {
+        Some(s) => s.to_string(),
+        None => String::from_utf8_lossy(lb.name_bytes()).to_string(),
+    };
+    let msg = format!("Fast-Forward: Setting {} to id: {}", name, rc.id());
+    println!("{}", msg);
+    lb.set_target(rc.id(), &msg)?;
+    repo.set_head(&name)?;
+    repo.checkout_head(Some(
+        git2::build::CheckoutBuilder::default()
+            // For some reason the force is required to make the working directory actually get updated
+            // I suspect we should be adding some logic to handle dirty working directory states
+            // but this is just an example so maybe not.
+            .force(),
+    ))?;
+    Ok(())
+}
+
+pub fn merge<'a>(
+    repo: &'a Repository,
+    remote_branch: &str,
+    fetch_commit: git2::AnnotatedCommit<'a>,
+) -> anyhow::Result<()> {
+    // 1. do a merge analysis
+    let analysis = repo.merge_analysis(&[&fetch_commit])?;
+
+    // 2. Do the appropriate merge
+    if analysis.0.is_fast_forward() {
+        println!("Doing a fast forward");
+        // do a fast forward
+        let refname = format!("refs/heads/{}", remote_branch);
+        match repo.find_reference(&refname) {
+            Ok(mut r) => {
+                fast_forward(repo, &mut r, &fetch_commit)?;
+            }
+            Err(_) => {
+                // The branch doesn't exist so just set the reference to the
+                // commit directly. Usually this is because you are pulling
+                // into an empty repository.
+                repo.reference(
+                    &refname,
+                    fetch_commit.id(),
+                    true,
+                    &format!("Setting {} to {}", remote_branch, fetch_commit.id()),
+                )?;
+                repo.set_head(&refname)?;
+                repo.checkout_head(Some(
+                    git2::build::CheckoutBuilder::default()
+                        .allow_conflicts(true)
+                        .conflict_style_merge(true)
+                        .force(),
+                ))?;
+            }
+        };
+    } else if analysis.0.is_normal() {
+        return Err(anyhow!(
+            "Unable to merge upstream changes to local, no appropriate merge strategy found"
+        ));
+    } else {
+        println!("Nothing to do...");
+    }
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,65 @@
-#![allow(unused)]
-
 use actix_web::{
     http::header::HeaderValue,
     web::{self, Bytes},
     App, HttpRequest, HttpResponse, HttpServer,
 };
-use anyhow::anyhow;
+use anyhow::{anyhow, Context};
 use git2::Repository;
 use hmac::{Hmac, Mac};
 use sha2::Sha256;
-use std::process::{Command, ExitStatus, Output, Stdio};
+use std::str::FromStr;
+use std::{
+    collections::HashMap,
+    ffi::OsStr,
+    path::PathBuf,
+    process::{ExitStatus, Stdio},
+};
+use tokio::{
+    io::{AsyncBufReadExt, BufReader},
+    process::Command as TokioCommand,
+};
 use tokio_stream::StreamExt;
 use tracing_actix_web::TracingLogger;
 
-use tokio::{
-    io::{AsyncBufRead, AsyncBufReadExt, BufReader},
-    process::Command as TokioCommand,
-};
-
 use tracing::log::*;
 
+mod git;
 mod model;
 mod span;
 
-use crate::span::CustomRootSpanBuilder;
+use crate::{
+    model::{GetReleaseRoot, UploadReleaseAssetRoot},
+    span::CustomRootSpanBuilder,
+};
+
+const VERIFY_GITHUB_SIGNATURE: bool = false;
+
+const USER_AGENT: &str = "chatterino-macos-artifact-builder 0.1.0";
 
 const WEBHOOK_SECRET: &[u8] = "penis123".as_bytes();
+const REPO_OWNER: &str = "pajlada";
+const REPO_NAME: &str = "chatterino2";
+const REPO_FULL_NAME: &str = const_format::formatcp!("{REPO_OWNER}/{REPO_NAME}");
+
+// The release we want to upload our asset to
+const RELEASE_ID: u64 = 82423741;
 
 const MAIN_BRANCH_REF: &str = "refs/heads/master";
-const REPO_FULL_NAME: &str = "pajlada/test";
+
+lazy_static::lazy_static! {
+    static ref GITHUB_CLIENT: reqwest::Client = {
+        let token = std::env::var("GITHUB_TOKEN").expect("Must have a GITHUB_TOKEN environment variable set");
+
+        let authorization_value: String = format!("Bearer {token}");
+
+        let mut default_headers = reqwest::header::HeaderMap::new();
+        default_headers.insert("User-Agent", reqwest::header::HeaderValue::from_static(USER_AGENT));
+        default_headers.insert("Accept", reqwest::header::HeaderValue::from_static("application/vnd.github+json"));
+        default_headers.insert("Authorization", reqwest::header::HeaderValue::from_str(authorization_value.as_str()).unwrap());
+
+        reqwest::Client::builder().default_headers(default_headers).build().unwrap()
+    };
+}
 
 fn get_hub_signature(hv: Option<&HeaderValue>) -> Result<Vec<u8>, actix_web::Error> {
     match hv {
@@ -64,11 +95,59 @@ fn validate_hub_signature(
     Ok(())
 }
 
+async fn build_and_upload_asset() -> anyhow::Result<()> {
+    info!("Start build");
+    let (artifact_path, asset_name) =
+        start_build("/tmp/artifact-builder", "Chatterino/chatterino2")
+            .await
+            .context("Failed building")?;
+    info!("Finished building - the build exists at {artifact_path:?}!");
+
+    // 1. Delete the macOS asset if it already exists
+    let old_macos_release_asset = find_macos_asset(REPO_OWNER, REPO_NAME, RELEASE_ID, &asset_name)
+        .await
+        .context("Finding macOS asset")?;
+
+    if let Some(asset_id) = old_macos_release_asset {
+        delete_github_asset(REPO_OWNER, REPO_NAME, asset_id)
+            .await
+            .context("Deleting macOS asset")?;
+    }
+
+    let release_asset = upload_asset_to_github_release(
+        REPO_OWNER,
+        REPO_NAME,
+        RELEASE_ID,
+        artifact_path,
+        &asset_name,
+    )
+    .await
+    .context("Uploading macOS asset")?;
+
+    info!(
+        "Successfully uploaded release asset {asset_name} to {}",
+        release_asset.browser_download_url
+    );
+
+    Ok(())
+}
+
 #[tracing::instrument(skip(bytes, req))]
 async fn new_build(req: HttpRequest, bytes: Bytes) -> actix_web::Result<actix_web::HttpResponse> {
-    let signature = get_hub_signature(req.headers().get("x-hub-signature-256"))?;
+    if VERIFY_GITHUB_SIGNATURE {
+        let signature = get_hub_signature(req.headers().get("x-hub-signature-256"))?;
 
-    validate_hub_signature(signature, &bytes, WEBHOOK_SECRET)?;
+        validate_hub_signature(signature, &bytes, WEBHOOK_SECRET)?;
+    }
+
+    // TODO: specify commit
+    tokio::spawn(async {
+        let res = build_and_upload_asset().await;
+
+        if let Err(e) = res {
+            info!("Error building/uploading asset: {e}");
+        }
+    });
 
     let body: model::Root =
         serde_json::from_slice(&bytes).map_err(actix_web::error::ErrorBadRequest)?;
@@ -87,39 +166,34 @@ async fn new_build(req: HttpRequest, bytes: Bytes) -> actix_web::Result<actix_we
         )));
     }
 
-    tracing::info!("make build job");
-
-    // TODO: specify commit
-    tokio::spawn(async {
-        match start_build("/tmp/artifact-builder", "Chatterino/chatterino2").await {
-            Ok(_) => {
-                info!("Finished building!");
-            }
-
-            Err(e) => {
-                info!("Building failed: {e}");
-            }
-        }
-    });
-
     Ok(HttpResponse::Ok().body("forsen"))
 }
 
 #[tracing::instrument(skip())]
-async fn run_command(command: &str) -> anyhow::Result<()> {
-    let mut child = TokioCommand::new("sh")
-        .arg("-c")
-        .arg(command)
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .spawn()?;
+async fn run_command<Cmd>(command: Cmd, envs: Option<HashMap<String, String>>) -> anyhow::Result<()>
+where
+    Cmd: AsRef<OsStr> + std::fmt::Debug,
+{
+    let mut cmd = TokioCommand::new("sh");
+
+    cmd.arg("-c");
+    cmd.arg(command);
+
+    cmd.stdout(Stdio::piped());
+    cmd.stderr(Stdio::piped());
+
+    if let Some(envs) = envs {
+        cmd.envs(envs);
+    }
+
+    let mut child = cmd.spawn()?;
 
     let stdout = child.stdout.take().unwrap();
-    let mut stdout_reader = BufReader::new(stdout).lines();
+    let stdout_reader = BufReader::new(stdout).lines();
     let mut stdout_reader_stream = tokio_stream::wrappers::LinesStream::new(stdout_reader);
 
     let stderr = child.stderr.take().unwrap();
-    let mut stderr_reader = BufReader::new(stderr).lines();
+    let stderr_reader = BufReader::new(stderr).lines();
     let mut stderr_reader_stream = tokio_stream::wrappers::LinesStream::new(stderr_reader);
 
     let handle: tokio::task::JoinHandle<Result<ExitStatus, std::io::Error>> =
@@ -152,50 +226,84 @@ async fn run_command(command: &str) -> anyhow::Result<()> {
     }
 }
 
-async fn start_build(clone_dir_str: &str, repo_full_name: &str) -> anyhow::Result<()> {
+async fn start_build(
+    clone_dir_str: &str,
+    repo_full_name: &str,
+) -> anyhow::Result<(PathBuf, String)> {
     let clone_dir = std::path::Path::new(clone_dir_str);
     let url = format!("https://github.com/{repo_full_name}");
 
+    /*
     if let Err(e) = std::fs::remove_dir_all(clone_dir) {
         // Don't error out if the directory we want to delete doesn't exist
         if e.kind() != std::io::ErrorKind::NotFound {
             return Err(anyhow::anyhow!(e));
         }
     }
+    */
 
-    std::fs::create_dir_all(clone_dir)?;
+    {
+        let r = Repository::open(clone_dir);
+        if let Ok(repo) = r {
+            info!("Using already-existing repo");
+            {
+                let mut remote = repo.find_remote("origin")?;
+                info!("Repo remote: {:?}", remote.name());
 
-    let repo = Repository::clone(&url, clone_dir)?;
+                let fetch_commit = git::fetch(&repo, &["master"], &mut remote)?;
 
-    info!("Cloned to {clone_dir:?}");
-
-    for mut submodule in repo.submodules()? {
-        info!("Cloning submodule {:?}", submodule.name());
-        submodule.update(true, None)?;
-    }
+                git::merge(&repo, "master", fetch_commit)?;
+            }
+            // TODO: update repo somehow
+            repo
+        } else {
+            info!("Cloning to {clone_dir:?}");
+            std::fs::create_dir_all(clone_dir)?;
+            let repo = Repository::clone_recurse(&url, clone_dir)?;
+            info!("Cloned to {clone_dir:?}");
+            repo
+        }
+    };
 
     // Build chatterino 4Head
     let build_dir = clone_dir.join("build");
+
+    if let Err(e) = std::fs::remove_dir_all(&build_dir) {
+        // Don't error out if the directory we want to delete doesn't exist
+        if e.kind() != std::io::ErrorKind::NotFound {
+            return Err(anyhow::anyhow!(e));
+        }
+    }
+
     std::fs::create_dir_all(&build_dir)?;
     std::env::set_current_dir(&build_dir)?;
 
     let qt_version = "5.15.8";
+    let qt_dir = "/opt/homebrew/opt/qt@5";
+    let openssl_root_dir = "/opt/homebrew/opt/openssl@1.1";
 
-    run_command("cmake ..").await?;
-    run_command("make -j8").await?;
-    run_command(&format!("../.CI/CreateDMG.sh {qt_version}")).await?;
+    let cmake_command =
+        format!("cmake -DUSE_PRECOMPILED_HEADERS=OFF -DCMAKE_PREFIX_PATH={qt_dir} -DOPENSSL_ROOT_DIR={openssl_root_dir} ..");
 
-    // TODO: Upload DMG
+    run_command(cmake_command, None).await?;
 
-    Ok(())
+    let build_command = "make -j8";
+    run_command(build_command, None).await?;
+
+    // TODO: Use codesign
+    let create_dmg_command = format!("../.CI/CreateDMG.sh {qt_version}");
+    let mut envs: HashMap<String, String> = HashMap::new();
+    envs.insert("Qt5_DIR".to_string(), qt_dir.to_string());
+    run_command(create_dmg_command, Some(envs)).await?;
+
+    let dmg_output = "chatterino-macos-Qt-5.15.8.dmg";
+
+    Ok((build_dir.join(dmg_output), dmg_output.to_string()))
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
-
-    // start build
-    // start_build("/tmp/artifact-builder", "Chatterino/chatterino2").await?;
 
     HttpServer::new(|| {
         let tracing_logger = TracingLogger::<CustomRootSpanBuilder>::new();
@@ -204,9 +312,70 @@ async fn main() -> anyhow::Result<()> {
             .wrap(actix_web::middleware::Logger::default())
             .service(web::resource("/new-build").to(new_build))
     })
-    .bind("127.0.0.1:8000")?
+    .bind("0.0.0.0:8000")?
     .run()
     .await?;
 
     Ok(())
+}
+
+async fn find_macos_asset(
+    owner: &str,
+    repo: &str,
+    release_id: u64,
+    asset_name: &str,
+) -> anyhow::Result<Option<u64>> {
+    let url = format!("https://api.github.com/repos/{owner}/{repo}/releases/{release_id}");
+    let res = GITHUB_CLIENT.get(url).send().await?.error_for_status()?;
+
+    let release: GetReleaseRoot = res.json().await?;
+
+    let macos_asset = release
+        .assets
+        .iter()
+        .find(|asset| asset.name == asset_name)
+        .map(|asset| asset.id);
+
+    Ok(macos_asset)
+}
+
+async fn delete_github_asset(owner: &str, repo: &str, asset_id: u64) -> anyhow::Result<()> {
+    let url = format!("https://api.github.com/repos/{owner}/{repo}/releases/assets/{asset_id}");
+
+    GITHUB_CLIENT.delete(url).send().await?.error_for_status()?;
+
+    Ok(())
+}
+
+async fn upload_asset_to_github_release(
+    owner: &str,
+    repo: &str,
+    release_id: u64,
+    path_to_file: PathBuf,
+    asset_name: &str,
+) -> anyhow::Result<UploadReleaseAssetRoot> {
+    let release_upload_url =
+        format!("https://uploads.github.com/repos/{owner}/{repo}/releases/{release_id}/assets");
+    let mut release_upload_url = url::Url::from_str(&release_upload_url)?;
+    release_upload_url.set_query(Some(format!("{}={}", "name", asset_name).as_str()));
+    // println!("upload_url: {}", release_upload_url);
+    let file_size = std::fs::metadata(&path_to_file)?.len();
+    println!(
+        "file_size: {}. It can take some time to upload. Wait...",
+        file_size
+    );
+    let file = tokio::fs::File::open(path_to_file).await?;
+
+    let res: UploadReleaseAssetRoot = GITHUB_CLIENT
+        .post(release_upload_url)
+        .header("Content-Type", "application/octet-stream")
+        .header("Content-Length", file_size.to_string())
+        .body(file)
+        .send()
+        .await?
+        .error_for_status()?
+        .json()
+        .await?;
+
+    Ok(res)
 }
